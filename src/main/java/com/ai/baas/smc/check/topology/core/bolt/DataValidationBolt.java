aab27ebe0ab799795bea1fb2d4ad841bcccf7ea4
myto2006 <myto2006@126.com>
2016-03-31 09:53:49 +0800

    no message

--- src/main/java/com/ai/baas/smc/check/topology/core/bolt/DataValidationBolt.java
+++ src/main/java/com/ai/baas/smc/check/topology/core/bolt/DataValidationBolt.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import com.ai.baas.smc.check.topology.constants.SmcCacheConstant;
 import com.ai.baas.smc.check.topology.vo.StlBillDetailStyleItem;
 import com.ai.baas.smc.check.topology.vo.StlBillStyle;
+import com.ai.baas.smc.check.topology.vo.StlElement;
 import com.ai.baas.smc.check.topology.vo.StlPolicy;
 import com.ai.baas.storm.message.MappingRule;
 import com.ai.baas.storm.message.MessageParser;
@@ -49,7 +50,9 @@ public class DataValidationBolt extends BaseBasicBolt {
     public void prepare(Map stormConf, TopologyContext context) {
         LOG.info("数据校验bolt[prepare方法]...");
         super.prepare(stormConf, context);
-        cacheClient = CacheClientFactory.getCacheClient(SmcCacheConstant.NameSpace.RULE_MANAGE);
+        if (cacheClient == null) {
+            cacheClient = CacheClientFactory.getCacheClient(SmcCacheConstant.NameSpace.RULE_MANAGE);
+        }
         /* 初始化hbase */
         HBaseProxy.loadResource(stormConf);
         /* 2.获取报文格式信息 */
@@ -79,7 +82,7 @@ public class DataValidationBolt extends BaseBasicBolt {
         String policyStr = cacheClient.hget(SmcCacheConstant.NameSpace.RULE_MANAGE, tenantId + "."
                 + policyCode);
         if (StringUtil.isBlank(policyStr)) {
-            throw new SystemException("政策定义不存在");
+            throw new SystemException("政策编码["+policyCode+"]定义不存在");
         }
         StlPolicy stlPolicy = JSON.parseObject(policyStr, StlPolicy.class);
         String billStyleSn = stlPolicy.getBillStyleSn();
@@ -87,11 +90,23 @@ public class DataValidationBolt extends BaseBasicBolt {
         String billDetailStyleStr = cacheClient.hget(SmcCacheConstant.NameSpace.RULE_MANAGE,
                 tenantId + "." + billStyleSn + "." + SmcCacheConstant.BILL_DETAIL_ITEM);
         if (StringUtil.isBlank(billDetailStyleStr)) {
-            throw new SystemException("详单项定义不存在");
+            throw new SystemException("账单样式编码["+billStyleSn+"]详单项定义不存在");
         }
         List<StlBillDetailStyleItem> stlBillDetailStyleItems = JSON.parseArray(billDetailStyleStr,
                 StlBillDetailStyleItem.class);
         /* 判断本详单数据是否满足详单项的格式定义（是否必填，取值类型）。 */
+        for(int i =0;i<stlBillDetailStyleItems.size();i++){
+            StlBillDetailStyleItem stlBillDetailStyleItem =stlBillDetailStyleItems.get(i);
+            Long elementId = stlBillDetailStyleItem.getElementId();
+            StringBuilder key = new StringBuilder();
+            key.append(stlBillDetailStyleItem.getTenantId()).append(".").append(elementId);
+            String elementStr =cacheClient.hget(SmcCacheConstant.NameSpace.RULE_MANAGE, key.toString());
+            if(StringUtil.isBlank(elementStr)){
+                throw new SystemException("元素ID["+elementId+"]不存在");
+            }
+            StlElement stlElement =JSON.parseObject(elementStr, StlElement.class);
+            
+        }
         /* 如果不满足则记录原因，沉淀数据库（在对应的详单数据记录后面增加一下校验结果【失败】及失败原因），并发送数据校验异常消息。 */
         /* 如果满足，则沉淀数据库（在对应的详单数据记录后面增加一下校验结果:通过），并发送数据校验通过消息。 */
         /* 更新结算详单校验计数器（redis）的校验通过记录数和校验失败记录数。 */
